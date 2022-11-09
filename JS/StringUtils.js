b8eccd76210fef5c142eb282e57186f8441cb736
cskwork <csk917@hotmail.com>
2022-11-10 08:15:35 +0900

    Update StringUtils.js

--- JS/StringUtils.js
+++ JS/StringUtils.js
@@ -1,11 +1,12 @@
+  //========================================================================  
 /*
-INPUT
-a,b,c,d,e
-OUTPUT
-a,b,c
-d,e
-*/
-  lineBreakBetweenLongText: (idString, lineBreakNumber)=>{
+  INPUT
+    a,b,c,d,e
+  OUTPUT
+    a,b,c
+    d,e
+  */
+  const lineBreakBetweenLongText = (idString, lineBreakNumber) => {
       let idList = idString.split(',');
       let newIdString = '';
       for (var i = idList.length - 1; i >= 0; i--) {
@@ -13,3 +14,34 @@ d,e
                     
       }
   },
+  //========================================================================
+
+  /*
+  INPUT
+    var arr = [
+      25,
+      "apple",
+      ["banana", "strawberry", "apple", 25]
+    ];
+  OUTPUT
+  2
+  */
+  const numberOfItems = (arr, item) => {
+    console.time('flat');
+    let count = 0;
+
+    flatDeep(arr, 10).forEach( eachItem =>{
+      if( eachItem === item){
+        count++;
+      }
+    });
+    // OR arr.flat(10)
+    console.timeEnd('flat');
+    return count;
+  }
+
+  const flatDeep = (arr, depth = 1) => {
+    return depth > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, depth - 1) : val), [])
+        : arr.slice();
+  };
+  //========================================================================
