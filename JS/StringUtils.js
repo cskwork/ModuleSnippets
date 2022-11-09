57138bb1885b21d4b6109a80537240dececb8f10
cskwork <csk917@hotmail.com>
2022-11-10 08:41:41 +0900

    Update StringUtils.js

--- JS/StringUtils.js
+++ JS/StringUtils.js
@@ -56,4 +56,22 @@
   };
   console.log(replaceAllInString('ab,###asf', '#', ''));
   //========================================================================
+  const sortJSONStringByPriceAscending = (jsonString) => {
+     const parsedJson =  JSON.parse(jsonString);
 
+     parsedJson.sort(function(a, b) {
+         let priceA = parseFloat(a.price);
+         let priceB = parseFloat(b.price);
+         if(priceA == priceB){ // sort by name
+            if(a.name < b.name) { return -1; }
+            if(a.name > b.name) { return 1; }
+            return 0;
+         }else{
+            return priceA - priceB;
+         }  
+     });
+
+     return JSON.stringify(parsedJson); 
+  }
+
+  console.log(sortJSONStringByPriceAscending('[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'));
