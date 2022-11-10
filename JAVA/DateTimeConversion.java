f5ac00c81cc8d5ef2fc203ea5366c60f9384aba4
cskwork <csk917@hotmail.com>
2022-11-10 14:53:34 +0900

    Update DateTimeConversion.java

--- JAVA/DateTimeConversion.java
+++ JAVA/DateTimeConversion.java
@@ -11,7 +11,32 @@
       return formatDateTime;
   };
 // ========================================================================================================================
-
+  /**
+  INPUT
+    String dateTime = "1995-07-25 15:03:21.000";
+    LocalDateTime date  = convertStringFormatToLocalDateTime(dateTime , "yyyy-MM-dd HH:mm:ss.SSS");
+  OUTPUT
+    After : 1995-07-25T15:03:21
+  */
+  public static LocalDateTime convertStringFormatToLocalDateTime(String stringDate , String stringPattern){
+        boolean isValidStringPattern = (
+            stringPattern.equals("yyyy-MM-dd HH:mm") 
+            || stringPattern.equals("yyyy-MM-dd HH:mm:ss")
+            || stringPattern.equals("yyyy-MM-dd HH:mm:ss.SSS")
+        );
+        boolean isValidStringDate = ( stringDate.length() == stringPattern.length() );
+        
+        if(!isValidStringPattern){
+             System.out.println(stringPattern+ " = NOT VALID PATTERN");
+        }
+        if(!isValidStringDate){
+             System.out.println(stringDate+ " = NOT VALID DATE");
+        }
+        
+        DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern(stringPattern);
+        LocalDateTime date = LocalDateTime.parse(stringDate, dateTimeFormat);
+        return date;
+   }
 
 
 
