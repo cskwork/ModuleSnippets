2bc4fb429ef01ffdfeea98dbc89c4ed7925083d6
cskwork <csk917@hotmail.com>
2022-11-11 14:05:45 +0900

    Update DateUtils.js

--- JS/DateUtils.js
+++ JS/DateUtils.js
@@ -8,3 +8,11 @@
        let time = local.toJSON().slice(0,10) +' '+  local.toJSON().slice(11,16);
        return time;
     });
+    Date.prototype.toDatePeriodInputValue = (function() {
+        let inputTime;
+        let local = new Date(this);
+        local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+        let timeStart = local.toJSON().slice(0,10); 
+        inputTime =timeStart +' ~ '+timeStart;
+        return inputTime;
+     });
