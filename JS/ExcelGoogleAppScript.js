d69f985d76cc4b2454e15ac528f5ba3ee3096005
cskwork <csk917@hotmail.com>
2022-11-25 10:32:17 +0900

    Update ExcelGoogleAppScript.js

--- JS/ExcelGoogleAppScript.js
+++ JS/ExcelGoogleAppScript.js
@@ -3,6 +3,8 @@
 // 입력시 조건에 따른 오늘의 날짜 고정해서 넣기 
 // https://stackoverflow.com/questions/16089041/how-can-i-test-a-trigger-function-in-gas/16089067#16089067
 function onEdit(e) {
+  const checkConditionColumnNumber = 6;
+  const setAutoDateColumnNumber = 5;
    // FOR DEBUG
    /*
    var debug_e = {
@@ -16,22 +18,20 @@ function onEdit(e) {
   console.log(debug_e);
   */
   var sheet = e.source.getActiveSheet();
-
-  if (e.range.getColumn() != 5) {
+  if (e.range.getColumn() != checkConditionColumnNumber) {
     return;
   }
   var inputCell = sheet.getRange(e.range.getRow(), e.range.getColumn()); // UTF-8 INPUT REQ. e.value is undefined
-  var cell = sheet.getRange(e.range.getRow(), 4);
-  //console.log('cell : ' + cell +e.range.getRow());
-  // If the cell is checked, it gets the adjaecent cell in column D and sets its number formating to the format 11-Nov-2020
-  //console.log('e.getValue '+ inputCell.getValue());
-
+  var cell = sheet.getRange(e.range.getRow(), setAutoDateColumnNumber);
+  
   if (inputCell.getValue() == "완료") {  
-    cell.setNumberFormat("yyyy.mm.dd HH:mm:ss").setValue(new Date());
+    let timeZone = Utilities.formatDate(new Date(), Session.getScriptTimeZone() , "yyyy.MM.dd HH:mm:ss");
+    // Logger.log(timeZone);
+    cell.setNumberFormat("yyyy.mm.dd HH:mm:ss").setValue(timeZone); 
   }
   // If the cell is unchecked, it will clear the ajaecent cell in column D.
-  else if (inputCell.getValue() != "완료") {
-    //cell.clear();
+  else if (inputCell.getValue() != "완료" && inputCell.getValue() != "종료") {
+     cell.clear();
   }
   //console.log('cellEND : ');
 }
