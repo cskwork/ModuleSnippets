2dfbb9f0414f5827411efa0635a92f61f3ad85ba
cskwork <csk917@hotmail.com>
2022-11-28 15:39:31 +0900

    Update DOMHandle.js

--- JS/DOMHandle.js
+++ JS/DOMHandle.js
@@ -8,3 +8,15 @@ const clickBindHtmlIdTag = (tagId, actionFunction) => {
 	let tagIdEvent = document.getElementById(tagId);
 	if(tagIdEvent) tagIdEvent.addEventListener("click", actionFunction);
 },
+/** 
+* setValuesWithClassTag
+* (Foreach X for compatibility)
+*/
+const setValuesWithClassTag = (value, className) =>{
+	let classTags = document.getElementsByClassName(className);
+	for(var i = 0, max = classTags.length; i < max; i++) 
+	{
+		classTags[i].style.display = "none";
+		classTags[i].innerHTML = '';
+	}	
+}
