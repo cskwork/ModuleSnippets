21ac939979faee1e2c90a048e41fb74d2d328170
cskwork <csk917@hotmail.com>
2022-12-06 07:56:00 +0900

    Update DataStructureManipulation.java

--- JAVA/DataStructureManipulation.java
+++ JAVA/DataStructureManipulation.java
@@ -1,19 +1,19 @@
 
 // Combine HashMap Lists
 public static List<HashMap> mergeHashMapLists(List<HashMap> map1, List<HashMap> map2){
-		List<HashMap> resultCombinedMap = new ArrayList<HashMap>();
-		
-		if(map1.size() != map1.size()) {
-			return resultCombinedMap;
-		}
-		
-		for (int i = 0; i < map1.size(); i++) {
-			String key1 = (String) map1.get(i).get("KEYNAME");
-			String key2 = (String) map2.get(i).get("KEYNAME");
-			if( key1.equals(key2)) {
-				resultCombinedMap.add(map1.get(i));
-				
-			}
-		}	
+	List<HashMap> resultCombinedMap = new ArrayList<HashMap>();
+	
+	if(map1.size() != map1.size()) {
 		return resultCombinedMap;
 	}
+	
+	for (int i = 0; i < map1.size(); i++) {
+		String key1 = (String) map1.get(i).get("KEYNAME");
+		String key2 = (String) map2.get(i).get("KEYNAME");
+		if( key1.equals(key2)) {
+			resultCombinedMap.add(map1.get(i));
+			
+		}
+	}	
+	return resultCombinedMap;
+}
